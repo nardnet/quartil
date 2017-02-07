2b2aaec559ad1cac3f674394e155382eac84b01b
nardnet <nardnet@gmail.com>
2017-02-07 21:01:37 -0200

    07/02/2017

---
+++ src/main/java/br/com/cobrarr/QuartilApplication.java
@@ -0,0 +1,12 @@
+package br.com.cobrarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartilApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/cobrarr/QuartilApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.cobrarr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartilApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
